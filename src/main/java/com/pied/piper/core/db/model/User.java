34156cae842dbb91743838a3d8e3b2782c34d074
Ankit Chaudhary <ankit.c@Chaudhary.local>
2016-07-21 22:01:55 +0530

    Added user controller and onetomany mapping in user to follower

--- src/main/java/com/pied/piper/core/db/model/User.java
+++ src/main/java/com/pied/piper/core/db/model/User.java
@@ -2,16 +2,14 @@ package com.pied.piper.core.db.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
 
 import io.dropwizard.jackson.JsonSnakeCase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by palash.v on 21/07/16.
@@ -36,5 +34,8 @@ public class User {
     @Column(name = "version")
     @Version
     private Integer version;
+
+    @OneToMany(mappedBy = "sourceUser", fetch = FetchType.LAZY)
+    private List<UserRelations> followers = new ArrayList<>();
 
 }
---
+++ src/main/java/com/pied/piper/resources/UserController.java
@@ -0,0 +1,13 @@
+package com.pied.piper.resources;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by ankit.c on 21/07/16.
+ */
+@Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
+public class UserController {
+}
